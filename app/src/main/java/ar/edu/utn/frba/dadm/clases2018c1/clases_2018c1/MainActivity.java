d0a4a1847d6f81e985a22e0abf08c055e778e79e
Juan Brito <juan.a.brito90@gmail.com>
2018-03-27 23:23:36 -0300

    Empty Application

---
+++ app/src/main/java/ar/edu/utn/frba/dadm/clases2018c1/clases_2018c1/MainActivity.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frba.dadm.clases2018c1.clases_2018c1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
