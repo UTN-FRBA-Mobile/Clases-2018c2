ff856f7c751a6861444c1b734ad1ebc788a9cb27
Juan Brito <juan.a.brito90@gmail.com>
2018-08-28 17:54:47 -0300

    Proyecto base (Vacio)

---
+++ app/src/main/java/ar/edu/utn/frba/dadm/clases2018c2/clases_2018c2/MainActivity.kt
@@ -0,0 +1,12 @@
+package ar.edu.utn.frba.dadm.clases2018c2.clases_2018c2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
