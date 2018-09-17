7f6d40a7d2f3dcbce8b62567deb12eb85957dac7
Juan Brito <juan.a.brito90@gmail.com>
2018-09-17 00:30:35 -0300

    Revert "Clase 5: - Ejemplos de uso de persistencia en File System - Ejemplo de persistencia en Preferencias - Ejemplo de persistencia en Base de Datos - Preparación de ejercicio práctico"
    This reverts commit 06b467f

--- app/src/main/java/ar/edu/utn/frba/dadm/clases2018c2/clases_2018c2/api/responses/MovieSearch.kt
+++
@@ -1,9 +0,0 @@
-package ar.edu.utn.frba.dadm.clases2018c2.clases_2018c2.api.responses
-
-import java.io.Serializable
-
-class MovieSearch : Serializable {
-    var response: Boolean = false
-    var search: List<Movie>? = null
-    var totalResults: String? = null
-}
